ff8bcb054b0c44a9d3d8178b8f8c94f4cfb284c8
naohiro <k14053kk@aitech.ac.jp>
2016-12-06 19:00:01 +0900

    first commit

---
+++ cameraTest/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Xamarin.Forms;
+
+namespace cameraTest
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new cameraTestPage()
+			{
+				Content = new Label
+				{
+					Text = "Hello, Forms !",
+					VerticalOptions = LayoutOptions.CenterAndExpand,
+					HorizontalOptions = LayoutOptions.CenterAndExpand,
+				},
+			};
+
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ cameraTest/cameraTestPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace cameraTest
+{
+	public partial class cameraTestPage : ContentPage
+	{
+		public cameraTestPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
